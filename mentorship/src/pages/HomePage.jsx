5074fec66567060bdbc0a4077df629eccb74a21f
Oriyan Nadav <oriyanadav5@gmail.com>
2023-10-10 18:07:21 +1000

    home page starter code

---
+++ mentorship/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+// import { useRef} from 'react';
+// import { Link } from "react-router-dom";
+
+import "./HomePage.css";
+
+
+function HomePage() {
+
+    return (
+        <section className="section home">
+        </section>
+    );
+}
+
+export default HomePage;
