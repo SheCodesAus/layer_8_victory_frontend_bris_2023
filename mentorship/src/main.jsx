47e49611fdb071e8e4ed79ec7f8c7158f3f1f018
Oriyan Nadav <oriyanadav5@gmail.com>
2023-10-10 20:00:11 +1000

    main

--- mentorship/src/main.jsx
+++ mentorship/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { AuthProvider } from './components/AuthProvider.jsx';
+import NavBar from "./components/NavBar.jsx";
+
+import HomePage from "./pages/HomePage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavBar />,
+    children: [
+      { path:"/", element: <HomePage /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+      </AuthProvider>
+  </React.StrictMode>
+);
