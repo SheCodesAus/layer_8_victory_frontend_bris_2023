e4248d94e01cc43f97f659b2a5cfebe8ee6ad582
Oriyan Nadav <oriyanadav5@gmail.com>
2023-10-20 11:44:39 +1000

    footer is ready

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,42 +2,50 @@ import { Link } from "react-router-dom";
 
 import './Footer.css';
 
-import FacebookIcon from '../../../public/Teal_MentorShip_Facebook.svg';
-import InstagramIcon from '../../../public/Teal_MentorShip_Instagram.svg';
-import LinkedInIcon from '../../../public/Teal_MentorShip_LinkedIn.svg';
-import SlackIcon from '../../../public/Teal_MentorShip_Slack.svg';
-import TwitterIcon from '../../../public/Teal_MentorShip_Twitter.svg';
+import FacebookIcon from '/Teal_MentorShip_Facebook.svg';
+import InstagramIcon from '/Teal_MentorShip_Instagram.svg';
+import LinkedInIcon from '/Teal_MentorShip_LinkedIn.svg';
+import SlackIcon from '/Teal_MentorShip_Slack.svg';
+import TwitterIcon from '/Teal_MentorShip_Twitter.svg';
 
 function Footer() {
 
     return (
-        <section>
+        <section className="footer">
+            <div className="footer-text">
+                <p>&copy; 2023 MentorShip</p>
+            </div>
             <footer>
                 <Link 
+                className="icon"
                 to="https://www.facebook.com/shecodesaustralia/" 
                 id="facebook"
                 >
                     <img src={FacebookIcon} alt="" />
                 </Link>
                 <Link 
+                className="icon"
                 to="https://www.instagram.com/shecodesaus/?hl=en" 
                 id="instagram"
                 >
                     <img src={InstagramIcon} alt="" />
                 </Link>
                 <Link 
+                className="icon"
                 to="https://www.linkedin.com/company/shecodesaustralia/" 
                 id="linkedin"
                 >
                     <img src={LinkedInIcon} alt="" />
                 </Link>
                 <Link 
+                className="icon"
                 to="https://join.slack.com/t/shecodesaus/shared_invite/enQtNzQ3ODk1OTQzODc3LTQ2ZWFmNzM0N[…]JkMWMyYWVjMDU3ZThhODJmMTk4YmRjZGMzZDRhZDBhODQwNzdlYmJhNDg1YjE" 
                 id="slack"
                 >
                     <img src={SlackIcon} alt="" />
                 </Link>
                 <Link 
+                className="icon"
                 to="https://twitter.com/shecodesaus" 
                 id="twitter"
                 >
