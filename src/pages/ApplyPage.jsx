14d8f26e6b1e929bd7d1209f46eb33a41263bbb4
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-21 16:42:40 +1000

    Adding redirection to apply page if user already logged in

--- src/pages/ApplyPage.jsx
+++ src/pages/ApplyPage.jsx
@@ -1,6 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import useAuth from "../hooks/use-auth";
 import ApplyForm from "../components/ApplyForm/ApplyForm";
 
 const ApplyPage = () =>{
+    const { auth } = useAuth();
+    const navigate = useNavigate();
+    useEffect(() => {
+      if (auth.token) {
+        navigate(`/events`);
+      }
+    }, []);
    
     return(
 
