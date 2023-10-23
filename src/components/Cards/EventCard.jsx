27721d1e0a8eb5a17ab5b63822a4848e4ec841bf
Andrea <andita54@hotmail.com>
2023-10-23 16:16:35 +1000

    css for event card

--- src/components/Cards/EventCard.jsx
+++ src/components/Cards/EventCard.jsx
@@ -1,14 +1,20 @@
-// import{ Link }from"react-router-dom";
 import "./Event_card.css";
+import Button from "../Buttton/Button";
+import { useNavigate } from "react-router-dom";
 
 const EventCard = (props) => {
 
+const navigate = useNavigate();
+const handleSendApply = ()=>{
+    navigate("/apply")
+}
 return(
     <>
-    <div className="card">
-        <h2>{props.EventData.title}</h2>
-        <p>Date: {props.EventData.start_date.split("T")[0]}</p>
-        <p>Location: {props.EventData.location}</p>
+    <div className="event-card">
+        <h2 className="event-title">{props.EventData.title}</h2>
+        <p className="event-text">Date: {props.EventData.start_date.split("T")[0]}</p>
+        <p className="event-text">Location: {props.EventData.location}</p>
+        <Button text={"Apply"} btnClass = "btn-info " onClick={handleSendApply}/>
     </div>
     </>
 )
