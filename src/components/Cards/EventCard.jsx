d7f8d11ca4943645d995c7b968ec7a7bb1de47c5
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-26 21:36:13 +1000

    Adding ability to register to events

--- src/components/Cards/EventCard.jsx
+++ src/components/Cards/EventCard.jsx
@@ -1,23 +1,44 @@
 import "./Event_card.css";
+import { useState } from "react";
+import postMentorEvents from "../../api/post-mentor-events";
+import useSelf from "../../hooks/use-self";
 import Button from "../Buttton/Button";
-import { useNavigate } from "react-router-dom";
 
 const EventCard = (props) => {
-    
-const navigate = useNavigate();
-const handleSendApply = ()=>{
-    navigate("/apply")
-}
-return(
-    <>
-    <div className="card">
-        <h2>{props.EventData.title}</h2>
-        <p>Date: {props.EventData.start_date.split("T")[0]}</p>
-        <p>Location: {props.EventData.location}</p>
-        <Button text={"Apply"} btnClass = "btn-info " onClick={handleSendApply}/>
-    </div>
-    </>
-)
-}
+    const { self, isLoading, error } = useSelf();
+    const [errorMessage, setErrorMessage] = useState("");
+
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>{error.message}</p>;
+    }
+
+    const handleSendApply = (event) => {
+        event.preventDefault();
+        setErrorMessage("");
+        postMentorEvents(props.EventData.id, self.id, false)
+        .then(() => {
+            window.alert("You have successfully registered for this event.");
+        })
+        .catch((error) => {
+            setErrorMessage(`${[error.message]}`);
+        });
+    };
+
+    return (
+        <>
+        <div className="card">
+            <h2>{props.EventData.title}</h2>
+            <p>Date: {props.EventData.start_date.split("T")[0]}</p>
+            <p>Location: {props.EventData.location}</p>
+            <Button text={"Apply"} btnClass="btn-info " onClick={handleSendApply} />
+            <div>{errorMessage}</div>
+        </div>
+        </>
+    );
+};
 
 export default EventCard;
