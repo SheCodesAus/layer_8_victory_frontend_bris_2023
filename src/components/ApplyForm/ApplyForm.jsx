108602e263f3d3a8b4c32a2a9989411ab83db77b
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-28 18:41:18 +1000

    Combining states of form validation errors

--- src/components/ApplyForm/ApplyForm.jsx
+++ src/components/ApplyForm/ApplyForm.jsx
@@ -11,255 +11,262 @@ import { emailIsValid } from "../../utlities/emailValidation";
 import "./ApplyForm.css";
 
 const ApplyForm = () => {
-  const navigate = useNavigate();
-  const { auth, setAuth } = useAuth();
+    const navigate = useNavigate();
+    const { auth, setAuth } = useAuth();
+    const [errorMessage, setErrorMessage] = useState("");
+    const [formInvalid, setFormInvalid] = useState("");
+    const [checkedState, setCheckedState] = useState([]);
+    const [mentored, setMentored] = useState(true);
+    const [formValidation] = useState({
+      urlError: "",
+      emailError: "",
+      mobileError: "",
+    });
+    const { skills, skillsLoading, skillsError } = useSkills([]);
+    const [signupdetails, setSignupDetails] = useState({
+      first_name: "",
+      last_name: "",
+      email: "",
+      mobile: "",
+      social_account: "",
+      linkedin_account: "",
+      github_profile: "",
+      username: "",
+      password: "",
+      has_mentored: false,
+      location: "",
+      skills: [],
+    });
 
-  const [errorMessage, setErrorMessage] = useState("");
-  const [formInvalid, setFormInvalid] = useState("");
-  const [checkedState, setCheckedState] = useState([]);
-  const [mentored, setMentored] = useState(true);
-  const [urlError, setUrlError] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const { skills, skillsLoading, skillsError } = useSkills([]);
-  const [signupdetails, setSignupDetails] = useState({
-    first_name: "",
-    last_name: "",
-    email: "",
-    mobile: "",
-    social_account: "",
-    linkedin_account: "",
-    github_profile: "",
-    username: "",
-    password: "",
-    has_mentored: false,
-    location: "",
-    skills: [],
-  });
-
-  if (skillsLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (skillsError) {
-    return <p>{skillsError.message}</p>;
-  }
-  
-  const handleChange = (event) => {
-    const { id, value } = event.target;
-    setSignupDetails((prevDetails) => ({
-      ...prevDetails,
-      [id]: value,
-    }));
-  };
-
-  const handleCheckboxChange = (event) => {
-    let updatedList = [...checkedState];
-    if (event.target.checked) {
-      updatedList = [...checkedState, event.target.value];
-    } else {
-      updatedList.splice(checkedState.indexOf(event.target.value), 1);
+    if (skillsLoading) {
+      return <p>Loading...</p>;
     }
-    setCheckedState(updatedList);
 
+    if (skillsError) {
+      return <p>{skillsError.message}</p>;
+    }
 
-    setSignupDetails({ ...signupdetails, skills: updatedList });
-  };
+    const handleChange = (event) => {
+      const { id, value } = event.target;
+      setSignupDetails((prevDetails) => ({
+        ...prevDetails,
+        [id]: value,
+      }));
+    };
 
-  const handleSelectionChange = (value) => {
-    setSignupDetails({ ...signupdetails, location: value });
-  };
-
-  const handleBooleanChange = (mentored) => {
-
-    setSignupDetails({ ...signupdetails, has_mentored: mentored });
-  
-  };
-
-  const handleSubmit = (event) => {
-
-    event.preventDefault();
-    setFormInvalid("");
-    setErrorMessage("");
-    setUrlError("");
-
-    const urls = [
-      signupdetails.social_account,
-      signupdetails.linkedin_account,
-      signupdetails.github_profile,
-    ];
-    for (let url in urls) {
-      if (urls[url] != "") {
-        if (isSchemeValid(urls[url]) && isUrlValid(urls[url])) {
-          setUrlError("");
-        } else {
-          setUrlError(
-            "Invalid URL, ensure you are including the protocol. Valid protocols include 'http', 'https', 'ftp' and 'ftps'"
-          );
-        }
+    const handleCheckboxChange = (event) => {
+      let updatedList = [...checkedState];
+      if (event.target.checked) {
+        updatedList = [...checkedState, event.target.value];
+      } else {
+        updatedList.splice(checkedState.indexOf(event.target.value), 1);
       }
-    }
-    if (emailIsValid(signupdetails.email)) {
-      setEmailError("");
-    } else {
-      setEmailError(
-        "Please provide a valid email address."
-      );
-    }
-    if (
-      signupdetails.first_name &&
-      signupdetails.last_name &&
-      signupdetails.email &&
-      signupdetails.mobile &&
-      signupdetails.username &&
-      signupdetails.password &&
-      signupdetails.location &&
-      signupdetails.skills
-    ) {
-      postCreateAccount(
-        signupdetails.first_name,
-        signupdetails.last_name,
-        signupdetails.email,
-        signupdetails.mobile,
+      setCheckedState(updatedList);
+      setSignupDetails({ ...signupdetails, skills: updatedList });
+    };
+
+    const handleSelectionChange = (value) => {
+      setSignupDetails({ ...signupdetails, location: value });
+    };
+
+    const handleBooleanChange = (mentored) => {
+      setSignupDetails({ ...signupdetails, has_mentored: mentored });
+    };
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      setFormInvalid("");
+      setErrorMessage("");
+      formValidation.urlError = "";
+      formValidation.emailError = "";
+
+      const urls = [
         signupdetails.social_account,
         signupdetails.linkedin_account,
         signupdetails.github_profile,
-        signupdetails.username,
-        signupdetails.password,
-        signupdetails.has_mentored,
-        signupdetails.location,
+      ];
+      for (let url in urls) {
+        if (urls[url] != "") {
+          if (isSchemeValid(urls[url]) && isUrlValid(urls[url])) {
+            formValidation.urlError = "";
+          } else {
+            formValidation.urlError =
+              "Invalid URL, ensure you are including the protocol. Valid protocols include 'http', 'https', 'ftp' and 'ftps'";
+          }
+        }
+      }
+      if (emailIsValid(signupdetails.email)) {
+        formValidation.emailError = "";
+      } else {
+        formValidation.emailError = "Please provide a valid email address.";
+      }
+      if (
+        signupdetails.first_name &&
+        signupdetails.last_name &&
+        signupdetails.email &&
+        signupdetails.mobile &&
+        signupdetails.username &&
+        signupdetails.password &&
+        signupdetails.location &&
         signupdetails.skills
-      )
-        .then(() => {
-          postLogin(signupdetails.username, signupdetails.password).then(
-            (response) => {
-              window.localStorage.setItem("token", response.token);
-              setAuth({
-                token: response.token,
-              });
-              navigate("/events");
-            }
-          );
-        })
-        .catch((error) => {
-          setErrorMessage(`${[error.message]}`);
-        });
-    } else {
-      setFormInvalid("Please complete all required fields and ensure you are entering a valid email address.");
-    }
-  };
+      ) {
+        postCreateAccount(
+          signupdetails.first_name,
+          signupdetails.last_name,
+          signupdetails.email,
+          signupdetails.mobile,
+          signupdetails.social_account,
+          signupdetails.linkedin_account,
+          signupdetails.github_profile,
+          signupdetails.username,
+          signupdetails.password,
+          signupdetails.has_mentored,
+          signupdetails.location,
+          signupdetails.skills
+        )
+          .then(() => {
+            postLogin(signupdetails.username, signupdetails.password).then(
+              (response) => {
+                window.localStorage.setItem("token", response.token);
+                setAuth({
+                  token: response.token,
+                });
+                navigate("/events");
+              }
+            );
+          })
+          .catch((error) => {
+            setErrorMessage(`${[error.message]}`);
+          });
+      } else {
+        setFormInvalid("Please complete all required fields.");
+      }
+    };
 
-  return (
-    <form className="apply-form background" id="form">
-      <img src="/background1.png" alt="" />
-      <div className="container-apply">
-        <div className="form-container form-container-apply">
-          <h1>Apply Now</h1>
-          <div className="normal-input">
-            <div className="input-container">
-              <label htmlFor="first_name">First Name<span className={formInvalid ? "" : "hidden"}>*</span> </label>
-              <br />
-              <input
-                className="text-input"
-                type="text"
-                id="first_name"
-                onChange={handleChange}
-                required
-              />
-            </div>
+    return (
+      <form className="apply-form background" id="form">
+        <img src="/background1.png" alt="" />
+        <div className="container-apply">
+          <div className="form-container form-container-apply">
+            <h1>Apply Now</h1>
+            <div className="normal-input">
+              <div className="input-container">
+                <label htmlFor="first_name">
+                  First Name<span className={formInvalid ? "" : "hidden"}>*</span>{" "}
+                </label>
+                <br />
+                <input
+                  className="text-input"
+                  type="text"
+                  id="first_name"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
 
-            <div className="input-container">
-              <label htmlFor="last_name">Last Name<span className={formInvalid ? "" : "hidden"}>*</span> </label>
-              <br />
-              <input
-                className="text-input"
-                type="text"
-                id="last_name"
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="input-container">
-              <label htmlFor="email">Email<span className={formInvalid ? "" : "hidden"}>*</span> </label>
-              <br />
-              <input
-                className="text-input"
-                type="email"
-                id="email"
-                onChange={handleChange}
-                required
-              />
-            </div>
+              <div className="input-container">
+                <label htmlFor="last_name">
+                  Last Name<span className={formInvalid ? "" : "hidden"}>*</span>{" "}
+                </label>
+                <br />
+                <input
+                  className="text-input"
+                  type="text"
+                  id="last_name"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="email">
+                  Email<span className={formInvalid ? "" : "hidden"}>*</span>{" "}
+                </label>
+                <br />
+                <input
+                  className="text-input"
+                  type="email"
+                  id="email"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
 
-            <div className="input-container">
-              <label htmlFor="mobile">Mobile<span className={formInvalid ? "" : "hidden"}>*</span> </label>
-              <br />
-              <input
-                className="text-input"
-                type="text"
-                id="mobile"
-                onChange={handleChange}
-                required
-              />
-            </div>
+              <div className="input-container">
+                <label htmlFor="mobile">
+                  Mobile<span className={formInvalid ? "" : "hidden"}>*</span>{" "}
+                </label>
+                <br />
+                <input
+                  className="text-input"
+                  type="text"
+                  id="mobile"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
 
-            <div className="input-container">
-              <label htmlFor="social_account">Social Account </label>
-              <br />
-              <input
-                className="text-input"
-                type="text"
-                id="social_account"
-                placeholder="url of your Social Account"
-                onChange={handleChange}
-              />
-            </div>
+              <div className="input-container">
+                <label htmlFor="social_account">Social Account </label>
+                <br />
+                <input
+                  className="text-input"
+                  type="text"
+                  id="social_account"
+                  placeholder="url of your Social Account"
+                  onChange={handleChange}
+                />
+              </div>
 
-            <div className="input-container">
-              <label htmlFor="linkedin_account">LinkedIn Account </label>
-              <br />
-              <input
-                className="text-input"
-                type="url"
-                id="linkedin_account"
-                placeholder="url of your LinkedIn profile"
-                onChange={handleChange}
-              />
-            </div>
+              <div className="input-container">
+                <label htmlFor="linkedin_account">LinkedIn Account </label>
+                <br />
+                <input
+                  className="text-input"
+                  type="url"
+                  id="linkedin_account"
+                  placeholder="url of your LinkedIn profile"
+                  onChange={handleChange}
+                />
+              </div>
 
-            <div className="input-container">
-              <label htmlFor="github_profile">GitHub </label>
-              <br />
-              <input
-                className="text-input"
-                type="text"
-                id="github_profile"
-                placeholder="url of your GitHub"
-                onChange={handleChange}
-              />
-            </div>
-            <div className="input-container">
-              <label htmlFor="username">Username<span className={formInvalid ? "" : "hidden"}>*</span> </label>
-              <br />
-              <input
-                className="text-input"
-                type="text"
-                id="username"
-                onChange={handleChange}
-                required
-              />
-            </div>
+              <div className="input-container">
+                <label htmlFor="github_profile">GitHub </label>
+                <br />
+                <input
+                  className="text-input"
+                  type="text"
+                  id="github_profile"
+                  placeholder="url of your GitHub"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="username">
+                  Username<span className={formInvalid ? "" : "hidden"}>*</span>{" "}
+                </label>
+                <br />
+                <input
+                  className="text-input"
+                  type="text"
+                  id="username"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
 
-            <div className="input-container">
-              <label htmlFor="password">Password<span className={formInvalid ? "" : "hidden"}>*</span> </label>
-              <br />
-              <input
-                className="text-input"
-                type="password"
-                id="password"
-                onChange={handleChange}
-                required
-              />
-            </div>
+              <div className="input-container">
+                <label htmlFor="password">
+                  Password<span className={formInvalid ? "" : "hidden"}>*</span>{" "}
+                </label>
+                <br />
+                <input
+                  className="text-input"
+                  type="password"
+                  id="password"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
               <div className="input-container input-checkbox">
                 <label htmlFor="mentor" className="label-checkbox">
                   Have you mentored with us before?{" "}
@@ -289,7 +296,9 @@ const ApplyForm = () => {
                 </div>
               </div>
               <div className="input-container input-checkbox">
-                <label htmlFor="Location ">Location<span className={formInvalid ? "" : "hidden"}>*</span> </label>
+                <label htmlFor="Location ">
+                  Location<span className={formInvalid ? "" : "hidden"}>*</span>{" "}
+                </label>
                 <br />
                 <div>
                   <Dropdown
@@ -309,7 +318,8 @@ const ApplyForm = () => {
 
               <div className="input-container input-checkbox">
                 <label htmlFor="skills" className="label-checkbox">
-                  Select skills<span className={formInvalid ? "" : "hidden"}>*</span>{" "}
+                  Select skills
+                  <span className={formInvalid ? "" : "hidden"}>*</span>{" "}
                 </label>
                 <div className="skills-checkbox-container">
                   {skills.map((item, index) => (
@@ -321,7 +331,11 @@ const ApplyForm = () => {
                         onChange={handleCheckboxChange}
                       />
 
-                      <label htmlFor={`skills-checkbox-${item}`} className="checkbox-text">{item}</label>
+                      <label
+                        htmlFor={`skills-checkbox-${item}`}
+                        className="checkbox-text">
+                        {item}
+                      </label>
                     </div>
                   ))}
                 </div>
@@ -329,21 +343,27 @@ const ApplyForm = () => {
             </div>
           </div>
 
-
           <div className="apply-button">
-              <Button text={"Submit"} btnClass="btn-info" onClick={handleSubmit} />
+            <Button text={"Submit"} btnClass="btn-info" onClick={handleSubmit} />
           </div>
           <div>
-          <p className={urlError || emailError ? "hidden" : ""}>{errorMessage}</p> 
+            <p
+              className={
+                formValidation.urlError || formValidation.emailError
+                  ? "hidden"
+                  : ""
+              }>
+              {errorMessage}
+            </p>
             {/* Filtering out generic error message if form validation errors are set*/}
             <sub className={errorMessage ? "hidden" : ""}>
               <p>{formInvalid}</p>
             </sub>
-            {<p>{urlError}</p>}
-            {<p>{emailError}</p>}
+            {<p>{formValidation.urlError}</p>}
+            {<p>{formValidation.emailError}</p>}
           </div>
         </div>
-    </form>
+      </form>
   );
 };
 
