2a336cf558ac78d4df07a39b31d2fdf7dc8677a9
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-10-31 12:23:43 +1000

    Revert "Merge pull request #50 from SheCodesAus/revert-47-navbarcss"
    This reverts commit 1d60fb9a1abf27ef2401edb4fcc0c49ae7648e62, reversing
    changes made to c4d3f79c8b820e256e018e4860784f6cf3292ff1.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -49,12 +49,7 @@ function NavBar() {
         <div>
             <div>
                 <header className="header">
-                    <img className="logo" src="../../../3.png" alt="" />
-                    <div className="menu-btn" onClick={toggleMenu}>
-                        <div className={`menu-burger ${menuOpen ? 'open' : ''}`}></div>
-                    </div>
-                    <nav className="navbar flex-nav" id="navbar">
-                        <Link 
+                <Link 
                         to="/" 
                         className={activeLink === "home" ? "active" : ""}
                         id="home"
@@ -63,114 +58,144 @@ function NavBar() {
                             handleLinkClick();
                         }}
                         >
-                            Home
+                            <img className="logo" src="/3.png" alt="" />
                         </Link>
-                        <Link 
-                        to="/about" 
-                        id="about"
-                        className={activeLink === "about" ? "active" : ""}
-                        onClick={() => {
-                            setActive("about");
-                            handleLinkClick();
-                        }}
-                        >
-                            About
-                        </Link>
-                        <Link 
-                        to="/events" 
-                        id="events"
-                        className={activeLink === "events" ? "active" : ""}
-                        onClick={() => {
-                            setActive("events");
-                            handleLinkClick();
-                        }}
-                        >
-                            Events
-                        </Link>
-                        <Link 
-                        to="/contact" 
-                        id="contact_us"
-                        className={activeLink === "contact" ? "active" : ""}
-                        onClick={() => {
-                            setActive("contact");
-                            handleLinkClick();
-                        }}
-                        >
-                            Contact
-                        </Link>
+                    <div className="menu-btn" onClick={toggleMenu}>
+                        <div className={`menu-burger ${menuOpen ? 'open' : ''}`}></div>
+                    </div>
+                    <nav className="navbar flex-nav" id="navbar">
+                        {!auth.is_staff ? (
+                            <>
+                                <Link 
+                                    to="/" 
+                                    className={activeLink === "home" ? "active" : ""}
+                                    id="home"
+                                    onClick={() => {
+                                        setActive("home");
+                                        handleLinkClick();
+                                    }}
+                                >
+                                    Home
+                                </Link>
+                                <Link 
+                                    to="/events" 
+                                    id="events"
+                                    className={activeLink === "events" ? "active" : ""}
+                                    onClick={() => {
+                                        setActive("events");
+                                        handleLinkClick();
+                                    }}
+                                >
+                                    Events
+                                </Link>
+                                <Link 
+                                    to="/about" 
+                                    id="about"
+                                    className={activeLink === "about" ? "active" : ""}
+                                    onClick={() => {
+                                        setActive("about");
+                                        handleLinkClick();
+                                    }}
+                                >
+                                    About
+                                </Link>
+                                <Link 
+                                    to="/contact" 
+                                    id="contact_us"
+                                    className={activeLink === "contact" ? "active" : ""}
+                                    onClick={() => {
+                                        setActive("contact");
+                                        handleLinkClick();
+                                    }}
+                                >
+                                    Contact
+                                </Link>
+
+                                {!auth.token ? 
+                                    <>
+                                        <Link 
+                                            to="/login"
+                                            id="login"
+                                            className={activeLink === "login" ? "active" : ""}
+                                            onClick={() => {
+                                                setActive("login");
+                                                handleLinkClick();
+                                            }}
+                                        >
+                                            Log In
+                                        </Link>
+                                        <Link 
+                                            to="/apply" 
+                                            id="apply"
+                                            className={activeLink === "apply" ? "active" : ""}
+                                            onClick={() => {
+                                                setActive("apply");
+                                                handleLinkClick();
+                                            }}
+                                        >
+                                            Apply
+                                        </Link>
+                                    </>
+                                    :
+                                    <></>
+                                }
+                            </>
+                        ):(
+                            <></>
+                        )}
                         
                         {auth.token ? (
                             <>
-                            <Link to="/" onClick={handleLogout}>
-                                Log Out
-                            </Link>
-                            
                             <Link 
-                            to="/profile" 
-                            id="profile"
-                            className={activeLink === "profile" ? "active" : ""}
-                            onClick={() => {
-                                setActive("profile");
-                                handleLinkClick();
-                            }}
+                                to="/profile" 
+                                id="profile"
+                                className={activeLink === "profile" ? "active" : ""}
+                                onClick={() => {
+                                    setActive("profile");
+                                    handleLinkClick();
+                                }}
                             >
                                 Profile
                             </Link>
+                            <Link 
+                                to="/" 
+                                onClick={handleLogout}
+                            >
+                                Log Out
+                            </Link>
 
-                            {auth.is_staff? 
-                             <><Link 
-                             to="/event-dashboard" 
-                             id="event-dashboard"
-                             className={activeLink === "event-dashboard" ? "active" : ""}
-                             onClick={() => {
-                                 setActive("event-dashboard");
-                                 handleLinkClick();
-                             }}
-                             >
-                                 Event Dashboard
-                             </Link>
-                             <Link 
-                             to="/mentor-dashboard" 
-                             id="mentor-dashboard"
-                             className={activeLink === "mentor-dashboard" ? "active" : ""}
-                             onClick={() => {
-                                 setActive("mentor-dashboard");
-                                 handleLinkClick();
-                             }}
-                             >
-                                 Mentor Dashboard
-                             </Link></>
+                            {auth.is_staff ? 
+                                <>
+                                    <Link 
+                                        to="/event-dashboard" 
+                                        id="event-dashboard"
+                                        className={activeLink === "event-dashboard" ? "active" : ""}
+                                        onClick={() => {
+                                            setActive("event-dashboard");
+                                            handleLinkClick();
+                                        }}
+                                    >
+                                        Event Dashboard
+                                    </Link>
+                                    <Link 
+                                        to="/mentor-dashboard" 
+                                        id="mentor-dashboard"
+                                        className={activeLink === "mentor-dashboard" ? "active" : ""}
+                                        onClick={() => {
+                                            setActive("mentor-dashboard");
+                                            handleLinkClick();
+                                        }}
+                                    >
+                                        Mentor Dashboard
+                                    </Link>
+                                </>
                             :
                             <></>
                             }
                             </>
 
                         ) : (
-                            <>
-                            <Link 
-                            to="/login"
-                            id="login"
-                            className={activeLink === "login" ? "active" : ""}
-                            onClick={() => {
-                                setActive("login");
-                                handleLinkClick();
-                            }}
-                            >
-                                Log In
-                            </Link>
-                            <Link 
-                            to="/apply" 
-                            id="apply"
-                            className={activeLink === "apply" ? "active" : ""}
-                            onClick={() => {
-                                setActive("apply");
-                                handleLinkClick();
-                            }}
-                            >
-                                Apply
-                            </Link>
-                            </>
-                            
+                            <></>
                         )}
                     </nav>
                 </header>
